c62424f1c3c10973c2e20b69e2f292c3ec16a25c
Alan Manoel <alanmanoel517@gmail.com>
2026-09-10 12:25:12 -0300

    adicionando dados em arquivos

---
+++ src/data/data.js
@@ -0,0 +1,10 @@
+import { TfiWorld } from "react-icons/tfi";
+
+
+export const LINKS = [
+        { name: "SOBRE", href: "#sobre" },
+        { name: "SERVIÇOS", href: "#servicos" },
+        { name: "PROJETOS", href: "#projetos" },
+        { name: "CONTATO", href: "#contato" },
+        { name: "PT", href: "#PT", icon: TfiWorld }
+    ];
