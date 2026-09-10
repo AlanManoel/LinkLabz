52bd5ea36802ef0bc4aca2e1222b4817d1a06ab6
Alan Manoel <alanmanoel517@gmail.com>
2026-09-10 13:00:05 -0300

    adicionando classes agora

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,9 +1,9 @@
 
 
 
-export const Button = ({ children, icon, href = "#" }) => {
+export const Button = ({ children, icon, href = "#", className = ""}) => {
     return (
-        <a href={href} className="flex justify-center items-center bg-[var(--orange-1)] text-[var(--white-1)] h-20 py-5 px-6 rounded a-bold btn-fade">
+        <a href={href} className={`flex justify-center items-center bg-[var(--orange-1)] text-[var(--white-1)] h-20 py-5 px-6 rounded a-bold btn-fade ${className}`}>
             {children}
             {icon && <span>{icon}</span>}
         </a>
