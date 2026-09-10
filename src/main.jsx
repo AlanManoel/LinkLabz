cb52fb039c08a2809e96d68e1bf1cde8620fe757
Alan Manoel <alanmanoel517@gmail.com>
2026-09-10 12:26:04 -0300

    adicionando responvidade

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './App.css'
-import Navbar from './components/Navbar'
-import Hero from './components/hero'
+import { Navbar } from './components/Navbar'
+import { Hero } from './components/Hero'
 import MarqueeServices from './components/MarqueeServices'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Navbar />
-    <Hero />
-    <MarqueeServices />
+    <Hero/>
+    {/* <MarqueeServices /> */}
   </StrictMode>,
 )
