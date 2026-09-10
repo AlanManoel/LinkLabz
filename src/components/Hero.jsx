cddbf854ad6a35869fb49fb08d28b5759697f381
Alan Manoel <alanmanoel517@gmail.com>
2026-09-10 12:48:33 -0300

    ajeitando a responsividade

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,11 +5,39 @@ import { IoIosArrowForward } from "react-icons/io";
 
 export const Hero = () => {
     return (
-        <div className="flex flex-col h-[calc(100vh-88px)] justify-center items-center md:gap-4 text-center px-8">
-            <h3 className="color-black-4">SOLUÇÕES WEB E MOBILE SOB MEDIDA</h3>
+        <div className="flex flex-col h-[calc(100vh-88px)] justify-center items-center md:gap-4 text-center px-8 max-sm:px-4 max-sm:gap-3">
+            <h3 className="color-black-4 max-sm:text-2xl">SOLUÇÕES WEB E MOBILE SOB MEDIDA</h3>
             <img src="img/logo-light.png" alt="imagem da logo branco" />
-            <h3 className="color-black-4 text-center max-w-4xl">CRIAMOS WEBSITES, APLICATIVOS E LANDING PAGES PARA IMPULSIONAR SEU NEGÓCIO.</h3>
+            <h3 className="color-black-4 text-center max-w-4xl max-sm:text-2xl">CRIAMOS WEBSITES, APLICATIVOS E LANDING PAGES PARA IMPULSIONAR SEU NEGÓCIO.</h3>
             <Button href="#contato" icon={<IoIosArrowForward size={32} />} >Entre em contato</Button>
         </div>
     )
 }
+
+// import { Button } from "./button/Button";
+// import { IoIosArrowForward } from "react-icons/io";
+
+// export const Hero = () => {
+//     return (
+//         <div className="flex flex-col h-[calc(100vh-88px)] justify-center items-center md:gap-4 text-center px-8 max-sm:px-4 max-sm:gap-3">
+//             <h3 className="color-black-4 max-sm:text-xl">
+//                 SOLUÇÕES WEB E MOBILE SOB MEDIDA
+//             </h3>
+//             <img
+//                 src="img/logo-light.png"
+//                 alt="imagem da logo branco"
+//                 className="max-sm:w-[300px]"
+//             />
+//             <h3 className="color-black-4 text-center max-w-4xl max-sm:text-xl max-sm:max-w-[360px]">
+//                 CRIAMOS WEBSITES, APLICATIVOS E LANDING PAGES PARA IMPULSIONAR SEU NEGÓCIO.
+//             </h3>
+//             <Button
+//                 href="#contato"
+//                 icon={<IoIosArrowForward size={32} />}
+//             >
+//                 Entre em contato
+//             </Button>
+
+//         </div>
+//     )
+// }
