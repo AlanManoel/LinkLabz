3e4259eed98fe7080afad71fa02d3fefd7fb1ffa
Alan Manoel <alanmanoel517@gmail.com>
2026-09-11 13:20:33 -0300

    adicionando os dados do marquee

--- src/data/data.js
+++ src/data/data.js
@@ -2,9 +2,24 @@ import { TfiWorld } from "react-icons/tfi";
 
 
 export const LINKS = [
-        { name: "SOBRE", href: "#sobre" },
-        { name: "SERVIÇOS", href: "#servicos" },
-        { name: "PROJETOS", href: "#projetos" },
-        { name: "CONTATO", href: "#contato" },
-        { name: "PT", href: "#PT", icon: TfiWorld }
-    ];
+    { name: "SOBRE", href: "#sobre" },
+    { name: "SERVIÇOS", href: "#servicos" },
+    { name: "PROJETOS", href: "#projetos" },
+    { name: "CONTATO", href: "#contato" },
+    { name: "PT", href: "#PT", icon: TfiWorld }
+];
+
+export const MARQUEE_ITEMS = [
+    "UI/UX DESIGN",
+    "WEBSITES",
+    "APLICATIVOS",
+    "LANDING PAGES",
+    "UI/UX DESIGN",
+    "WEBSITES",
+    "APLICATIVOS",
+    "LANDING PAGES",
+    "UI/UX DESIGN",
+    "WEBSITES",
+    "APLICATIVOS",
+    "LANDING PAGES",
+];
